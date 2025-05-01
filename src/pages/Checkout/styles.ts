9fbc9d47b59c76049670ab342150d3aed579ff0b
ThauanyAA <thauanyaraujo5@gmail.com>
2025-05-01 17:34:31 -0300

    Improving layout on Checkout page

--- src/pages/Checkout/styles.ts
+++ src/pages/Checkout/styles.ts
@@ -2,7 +2,7 @@ import { styled } from 'styled-components'
 
 export const CheckoutContainer = styled.div`
   display: grid;
-  grid-template-columns: 40rem 28rem;
+  grid-template-columns: 0.58fr 0.4fr;
   gap: 2rem;
   margin-top: 2rem;
   width: 100%;

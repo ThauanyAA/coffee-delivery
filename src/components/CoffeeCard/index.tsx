7c49360d53ee8b1826920484b9e330cf07447446
ThauanyAA <thauanyaraujo5@gmail.com>
2025-05-16 16:08:35 -0300

    Handle add item to cart

--- src/components/CoffeeCard/index.tsx
+++ src/components/CoffeeCard/index.tsx
@@ -1,3 +1,4 @@
+import { useContext, useState } from 'react';
 import { AddToCartButton, BuyContainer, Card, Image, Tag, TagsContainer } from "./styles";
 import Expresso from "../../assets/coffees/Expresso.png";
 import { ShoppingCartSimple } from "phosphor-react";
@@ -5,8 +6,36 @@ import { Coffee } from "../../@types/definitions";
 import { formatPriceToBRL } from "../../helpers/utils";
 import { InputNumber } from "../InputNumber";
 import { Text, Title } from "../Typography";
+import { CartContext } from '../../contexts/CartContext';
+import { toast } from 'react-toastify';
 
+const MIN_QTY_ITEM = 1
 export function CoffeeCard({ coffee }: { coffee: Coffee } ) {
+  const { addItemToCart } = useContext(CartContext)
+  const [quantity, setQuantity] = useState(MIN_QTY_ITEM);
+  
+
+  const incrementQty = () => {
+    if (quantity < coffee.quantity) {
+      setQuantity(quantity + 1);
+    } else {
+      toast.warning('Atingiu a quantidade máxima do estoque!');
+    }
+  };
+
+  const decrementQty = () => {
+    if (quantity > MIN_QTY_ITEM) setQuantity(quantity - 1);
+  };
+
+  const handleAddItemToCart = (item: Coffee) => {
+    const itemToAdd = {
+      name: item.name,
+      id: item.id,
+      price: item.price,
+      quantity
+    }
+    addItemToCart(itemToAdd)
+  }
   return (
     <Card>
       <Image src={Expresso} alt="Coffee" />
@@ -29,8 +58,12 @@ export function CoffeeCard({ coffee }: { coffee: Coffee } ) {
             R$<Title variant="m" color="base-text" inline>{formatPriceToBRL(coffee.price)}</Title>
           </Text>
         </div>
-        <InputNumber />
-        <AddToCartButton>
+        <InputNumber
+          value={quantity}
+          increment={incrementQty}
+          decrement={decrementQty}
+        />
+        <AddToCartButton onClick={() => handleAddItemToCart(coffee)}>
           <ShoppingCartSimple size={20} weight="fill" />
         </AddToCartButton>
       </BuyContainer>
--- src/components/InputNumber/index.tsx
+++ src/components/InputNumber/index.tsx
@@ -1,18 +1,13 @@
-import { useState } from 'react';
 import { InputBtn, InputNumberContainer } from './styles';
 import { Minus, Plus } from 'phosphor-react';
 
-export const InputNumber = ({ min = 1, max = 99 }) => {
-  const [value, setValue] = useState(1);
+interface InputNumberProps {
+  value: number
+  increment: () => void
+  decrement: () => void
+}
 
-  const increment = () => {
-    if (value < max) setValue(value + 1);
-  };
-
-  const decrement = () => {
-    if (value > min) setValue(value - 1);
-  };
-
+export const InputNumber = ({ value, increment, decrement }: InputNumberProps) => {
   return (
     <InputNumberContainer>
       <InputBtn onClick={decrement}>
